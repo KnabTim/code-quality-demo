ae2e5a912a0a51520f923137208f9e2b8ab4d7ed
KnabTim <tim.knab@outlook.com>
2026-06-29 11:13:41 +0200

    Update beispiel_fehlerhaft.py

--- beispiel_fehlerhaft.py
+++ beispiel_fehlerhaft.py
@@ -5,15 +5,15 @@ GITHUB_TOKEN = "ghp_aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789"
 
 
 # mehrere demonstrationen für Black, fehlende Leerzeichen
-def berechne_summe(a, b):
-    x = a + b
+def berechne_summe(a,b):
+    x=a+b
     return x
 
 
-def berechne_differenz(a, b):
-    x = a - b
+def berechne_differenz(a,b):
+    x=a-b
     return x
 
 
-if __name__ == "__main__":
-    print(berechne_summe(3, 4))
+if __name__=="__main__":
+    print(berechne_summe(3,4))
